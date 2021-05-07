3aad05fbef97b0704cea7504032a84a87e0df78c
N3v3rek <bojeska@gmail.com>
2021-05-07 20:28:44 +0200

    checkFile simplified, readFile added, writeFile to do

--- src/Java/FileOperations/FilesReader.java
+++ src/Java/FileOperations/FilesReader.java
@@ -1,16 +1,30 @@
 package Java.FileOperations;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-
 public class FilesReader {
 
-    public static boolean checkFile (String path) {
-            if (Files.exists(Paths.get(path))){
-                return true;
-            }
-            System.out.println("Nie znaleziono pliku o ścieżce: " + path);
-            return false;
+    public static boolean checkFile(String path) {
+        return Files.exists(Paths.get(path));
     }
+
+    public static String readFile(String path) throws IOException {
+        try {
+            BufferedReader bufferedReader = Files.newBufferedReader(Paths.get(path));
+            return bufferedReader.readLine();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            throw new FileNotFoundException("Nie znaleziono pliku o ścieżce: " + path);
+        } catch (IOException e){
+            e.printStackTrace();
+            throw new IOException("Wystapil jakis blad - zwiazany z z operacjami I/O");
+        }
+    }
+
+    //TODO
+    //Metoda zapisująca stringa w pliku -> zapis dysku parzystości
 }
