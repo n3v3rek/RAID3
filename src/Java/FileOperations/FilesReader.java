fddde036b01692f3145efdd2b0a482e493fc5c43
N3v3rek <bojeska@gmail.com>
2021-06-08 11:03:25 +0200

    adding createReport method to FilesReader

--- src/Java/FileOperations/FilesReader.java
+++ src/Java/FileOperations/FilesReader.java
@@ -1,11 +1,13 @@
 package Java.FileOperations;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import Java.Model.SetOfDisks;
+
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class FilesReader {
 
@@ -14,12 +16,71 @@ public class FilesReader {
         return Files.exists(filePath);
     }
 
-    public static String readFileLane(String path) throws IOException {
+    public static ArrayList<String> readFile(String path)
+        throws IOException{
         try {
             Path filePath = Paths.get("src\\Resources\\" + path);
             BufferedReader bufferedReader = Files.newBufferedReader(filePath);
-            return bufferedReader.readLine();
+            ArrayList<String> fileLine = bufferedReader.lines().collect(Collectors.toCollection(ArrayList::new));
+            bufferedReader.close();
+            return fileLine;
         } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            throw new FileNotFoundException("Nie znaleziono pliku o ścieżce: " + path);
+        } catch (IOException e){
+            e.printStackTrace();
+            throw new IOException("Wystapil jakis blad - zwiazany z z operacjami I/O");
+        }
+
+    }
+    public static String readFileLane(String path)
+            throws IOException {
+        try {
+            Path filePath = Paths.get("src\\Resources\\" + path);
+            BufferedReader bufferedReader = Files.newBufferedReader(filePath);
+            String fileLine = bufferedReader.readLine();
+            bufferedReader.close();
+            return fileLine;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            throw new FileNotFoundException("Nie znaleziono pliku o ścieżce: " + path);
+        } catch (IOException e){
+            e.printStackTrace();
+            throw new IOException("Wystapil jakis blad - zwiazany z z operacjami I/O");
+        }
+    }
+
+    public static void createReport(String path, SetOfDisks setOfDisks, String status)
+            throws IOException{
+        try {
+            FileWriter fileWriter = new FileWriter("src\\Resources\\" + path);
+
+            fileWriter.write("Disc One:\n");
+            if (setOfDisks.getDiscOne() != null){
+                for (String lane : setOfDisks.getDiscOne()){
+                    fileWriter.write(lane +"\n");
+                }
+            }else {
+                fileWriter.write("Disc one is not initialized.\n");
+            }
+
+            fileWriter.write("Disc Two:\n");
+            if (setOfDisks.getDiscTwo() != null) {
+                for (String lane : setOfDisks.getDiscTwo()) {
+                    fileWriter.write(lane + "\n");
+                }
+            }else {
+                fileWriter.write("Disc two is not initialized.\n");
+            }
+
+            fileWriter.write("Control Disc:\n");
+            for (String lane : setOfDisks.getControlDisc()){
+                fileWriter.write(lane+"\n");
+            }
+
+            fileWriter.write("Status - " + status);
+            fileWriter.close();
+        }catch (FileNotFoundException e) {
             e.printStackTrace();
             throw new FileNotFoundException("Nie znaleziono pliku o ścieżce: " + path);
         } catch (IOException e){
